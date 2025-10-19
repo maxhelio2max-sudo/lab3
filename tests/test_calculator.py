3bf55da2080f03e4f8338e5fb9b57eb63fbe53e2
maxhelio2max-sudo <max.helio2max@gmail.com>
2025-10-20 02:02:36 +0500

    was fixed mistakes

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,10 @@
-from calculator import add, multiply
+from calculator import add, multiply, subtract
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_multiply():
     assert multiply(2, 3) == 6
+
+def test_subtract():
+    assert subtract(5, 3) == 2
